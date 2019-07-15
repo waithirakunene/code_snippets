cb64780fc0a9ba5e31f35b798a22b29e6d2b5aca
Waithira <waithirakunene@users.noreply.github.com>
2019-07-15 15:54:35 +0300

    Update class.py

--- class.py
+++ class.py
@@ -1,17 +1,17 @@
 #create a class called MyClass
 class Myself():
-    #assign values to MyClass attributes
+    #assign values to class Myself attributes
     number = 0
     name = ""
 
 def Main(self):
-    #create an object for MyClass class
+    #create an object for class Myself 
     me = Myself()
-    #access the attributes of MyClass using (.) operator
+    #access the attributes of Myself class using (.) operator
     me.number = 1928
     me.name = "Waithira Kunene"
-
+    #print the values
     print(me.name + "" + me.number)
-
+#remind Python there is method Main
 if __name__ == '__name__':
     Main()
